a0d31cb775cdaf41908cce8008446a3ecfd44b5c
KhalilZ547 <zoghlamikhalil7@gmail.com>
2024-03-05 19:00:35 +0100

    update users model (Query Sql)

--- server/database-mysql/model/usersModel.js
+++ server/database-mysql/model/usersModel.js
@@ -1,26 +1,25 @@
 const connection = require("../index.js")
 
 const selectAll=()=>{
-  sql= "SELECT * FROM users"
+  const sql= "SELECT * FROM users"
   return connection.query(sql)
 }
 const loginUser = async (userName, password) => {
   const sql = 'SELECT * FROM users WHERE userName = ? AND password = ?';
-  const [results] = await connection.query(sql, [userName, password]);
+  const results= await connection.query(sql, [userName, password]);
   return results;
 };
 const addOne = (userName,password)=>{
-    sql = "INSERT into users (userName,password) values (?,?) "
+    const sql = "INSERT into users (userName,password) values (?,?) "
      return connection.query(sql,[userName,password])
   }
  
   const deleteU =(id)=>{
-    const userId = parseInt(id);
-    sql="DELETE FROM users WHERE idusers=?"
-    return connection.query(sql,[userId])
+   const sql="DELETE FROM users WHERE idusers=?"
+    return connection.query(sql,[id])
   }
   const updateU =(userName,password,id)=>{
-    sql="UPDATE users SET userName=?, password=? WHERE idusers =?"
+   const sql="UPDATE users SET userName=?, password=? WHERE idusers =?"
     return connection.query(sql,[userName,password,id])
   }
   
