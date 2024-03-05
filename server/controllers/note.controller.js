be3751d8f0dc53995d8acdd2c4ccee7468cc6db5
KhalilZ547 <zoghlamikhalil7@gmail.com>
2024-03-05 15:44:22 +0100

    update notes controller (handling async responses)

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -13,24 +13,24 @@ const select = async function (req, res) {
 
 const add = async (req, res) => {
   try {
-      await model.addOne(req.body.title,req.body.content); 
-      res.status(200).send("Note added successfully");
+  const response=  await model.addOne(req.body.title,req.body.content); 
+      res.status(200).send(response);
   } catch (error) {
       res.status(500).send(error);
   }
 };
 const deleteOne = async (req, res) => {
   try {
-      await model.deleteO(req.params.id);
-      res.status(200).send("Note deleted successfully");
+   const response =await model.deleteO(req.params.id);
+      res.status(200).send(response);
   } catch (error) {
       res.status(500).send(error);
   }
 };
 const updateOne = async (req, res) => {
   try {
-      await model.update(req.body.title,req.body.content, req.params.id); 
-      res.status(200).send("Note updated successfully");
+    const respone = await model.update(req.body.title,req.body.content, req.params.id); 
+      res.status(200).send(respone);
   } catch (error) {
       res.status(500).send(error);
   }
