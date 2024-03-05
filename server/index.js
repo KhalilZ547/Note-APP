909de10c87743be0118e94f755f78f9fe2cacf24
KhalilZ547 <zoghlamikhalil7@gmail.com>
2024-03-05 11:31:18 +0100

    The Express APP Server

--- server/index.js
+++ server/index.js
@@ -1,21 +1,20 @@
 const express = require("express");
-const itemRoutes = require('./routes/item.routes')
-
-// TODO: Update this
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
+const noteRoutes = require('./routes/note.routes.js')
+const userRoutes = require('./routes/user.routes.js')
 // const db = require('./database-mysql');
-// const db = require('./database-mongo');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../client/dist"));
 
-app.use("/api/items", itemRoutes);
+app.use("/api/notes", noteRoutes);
+app.use("/api/users",userRoutes)
 
-app.listen(PORT, function () {
-  console.log("listening on port 3000!");
+
+
+app.listen(PORT,()=>{
+  console.log(`listening on port : ${PORT}`);
 });
