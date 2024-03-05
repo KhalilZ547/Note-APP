568cc5cfc571837d266cba25b8e521b6816069b5
KhalilZ547 <zoghlamikhalil7@gmail.com>
2024-03-05 11:25:10 +0100

    mySql Database connection

--- server/database-mysql/index.js
+++ server/database-mysql/index.js
@@ -1,10 +1,21 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'mvp'
-});
+const connection = mysql
+  .createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "mvp",
+  })
+  .promise();
+  
+connection.connect()
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+ 
 
 module.exports = connection;
