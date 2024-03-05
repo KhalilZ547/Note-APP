48aa2b0121410cc7e2bb62a74f252261448789b7
KhalilZ547 <zoghlamikhalil7@gmail.com>
2024-03-05 19:01:45 +0100

    update App

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,29 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import $ from 'jquery'
-import List from './components/List.jsx'
+import NotesList from './components/noteList.jsx'
+import Login from './components/login.jsx'
+
 
 const App = () => {
-  const [items, setItems] = useState([])
-  useEffect(() => {
-    $.ajax({
-      url: '/api/items',
-      success: (data) => {
-        console.log(data)
-        setItems(data)
-      },
-      error: (err) => {
-        console.log('err', err)
-      },
-    })
-  }, [])
+  const [view,setView]=useState("login")
+  const [userid ,setUserId]=useState("")
+  const changeView = ()=>{
 
+  }
+  if (view==="login") {
   return (
     <div>
-      <h1>Item List</h1>
-      <List items={items} />
+      <h2 style={{background:"url('https://img.freepik.com/free-vector/realistic-style-technology-particle-background_23-2148426704.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709596800&semt=ais')",width:"102%",textAlign:"center",color:"white",marginBottom: "3px"}} >It's more than a Note.</h2>
+      <Login/>
     </div>
-  )
+  
+  )}else if (view==="notes") {
+    return (<div>
+       <h1>Your Notes</h1>
+      <NotesList id={userid}/>
+    </div>)
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
