49edb1e0343f85a83f633ff5f51569d926f2a5da
KhalilZ547 <zoghlamikhalil7@gmail.com>
2024-03-06 03:17:37 +0100

    working on handling fetching notes after user interaction (add)

--- client/src/components/noteList.jsx
+++ client/src/components/noteList.jsx
@@ -8,7 +8,10 @@ const NotesList = (props) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   useEffect(() => {
-    axios.get('http://localhost:3000/api/notes/'+props.id)
+    fetchNotes()
+  }, []);
+ const fetchNotes=()=>{
+  axios.get('http://localhost:3000/api/notes/'+props.id)
       .then((response) => {
         // console.log(props.id); 
         // console.log(response.data);
@@ -17,7 +20,7 @@ const NotesList = (props) => {
       .catch((error) => {
         console.error( error);
       });
-  }, []);
+ }
 
   const handleDeleteNote = (noteId) => {
     axios.delete(`http://localhost:3000/api/notes/${noteId}`)
@@ -37,7 +40,7 @@ const NotesList = (props) => {
   const handleAddNote = () => {
     axios.post(`http://localhost:3000/api/notes/${props.id}`,{title,content} )
     .then((response) => {
-      setNotes([notes, response.data]);
+      fetchNotes()
       setShowAddNote(false);
       setTitle('');
       setContent('');
@@ -46,10 +49,13 @@ const NotesList = (props) => {
       console.error(error);
     });
     }
+    
 
     return (
+      
       <div style={{ background: "url('https://img.freepik.com/free-vector/realistic-style-technology-particle-background_23-2148426704.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709596800&semt=ais')", border: " 2px solid gray", display: "flex", width: "100%", color: "white", padding: " 10px", cursor: "pointer", justifyContent: "space-around", boxShadow: "10px 5px 5px gray" }}>
         <h2>My Notes</h2>
+        
         <ul>
           {notes.map((note) => (
             <li key={note.id}>
