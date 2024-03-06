66ad0a2b2791d5369fef47919631399b719c5c04
KhalilZ547 <zoghlamikhalil7@gmail.com>
2024-03-06 01:09:25 +0100

    users route updated

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userController = require("../controllers/user.controller.js");
 
 router.get("/", userController.selectU);
-router.post("/signup", userController.addUser);
+router.post("/signup", userController.signup);
 router.post("/login",userController.login)
 router.delete("/:id",userController.deleteUser);
 router.put("/:id",userController.updateUser)
